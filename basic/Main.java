a8b7b2c1f23be86e8945225e91a8ea8e303f4e46
Aditya Gupta <adityagupta2253@gmail.com>
2024-12-24 01:47:07 +0530

    first file

---
+++ basic/Main.java
@@ -0,0 +1,11 @@
+package basic;
+public class Main {
+    public static void main(String args[]){
+    // first print a string
+    String username="aditya-gupta13";
+    
+
+    System.out.print(username);    
+
+    }
+}
